a5aa88dd36e7ee5006cedd57ea47afed9ae3f0a3
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-03-04 15:35:08 +0545

    Add Next.js files and configurations

---
+++ next-js/week2/app/page.tsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+interface Data  {
+  name:string,
+  email:string,
+}
+async function getUserDetails<Data>() {
+  // await new Promise((r)=> setTimeout(r,5000))  --delay promise
+  const response = await axios.get("http://localhost:3000/api/user")
+	return response.data;
+}
+
+export default async function Home() {
+  const userData = await getUserDetails()
+  return ( <div className="flex flex-col justify-center h-screen">
+  <div className="flex justify-center">
+      <div className="border p-8 rounded">
+          <div>
+              Name: {userData?.name}
+          </div>
+          
+          {userData?.email}
+      </div>
+  </div>
+</div>
+  )
+}
