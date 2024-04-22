02c4c1df60f1b147633c7ada625d02db0d7157e8
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-04-22 22:23:22 +0545

    next-auth

---
+++ next-auth/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default  async function Home() {
+//   const res = await fetch('https://sum-server.100xdevs.com/todos', {
+//     next: { revalidate: 10 }
+// }); static
+  return (
+   <div></div>
+  );
+}
---
+++ next-auth/middleware.ts
@@ -0,0 +1,31 @@
+// import { NextRequest, NextResponse } from "next/server";
+
+// let requestCount = 0;
+// export function middleware(req:NextRequest){
+// requestCount++
+
+// const res =NextResponse.next()
+// console.log(requestCount)
+// return res;
+// }
+
+// export const config = {
+//     matcher: '/api/:path*',
+
+//   }
+
+import { useEffect } from "react";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  
+  console.log(request.nextUrl.pathname);
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  if (request.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.next();
+  }
+}
